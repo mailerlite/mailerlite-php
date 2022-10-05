1235d3257f72c6713676dc19e748268ad5d52f27
Nikol Petrunova <npetrunova@gmail.com>
2022-10-05 16:27:06 +0300

    groups and segments

---
+++ src/Endpoints/Group.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace MailerLite\Endpoints;
+
+class Group extends AbstractEndpoint
+{
+    protected string $endpoint = 'groups';
+
+    public function create(array $params): array
+    {
+        return $this->httpLayer->post(
+            $this->buildUri($this->endpoint),
+            $params
+        );
+    }
+
+    public function find(string $groupId): array
+    {
+        return $this->httpLayer->get(
+            $this->buildUri($this->endpoint) . "/{$groupId}"
+        );
+    }
+
+    public function get(array $params): array
+    {
+        return $this->httpLayer->get(
+            $this->buildUri($this->endpoint, $params)
+        );
+    }
+
+    public function update(string $groupId, array $params): array
+    {
+        return $this->httpLayer->put(
+            $this->buildUri($this->endpoint) . "/{$groupId}",
+            $params
+        );
+    }
+
+    public function delete(string $groupId): array
+    {
+        return $this->httpLayer->delete(
+            $this->buildUri($this->endpoint . "/{$groupId}")
+        );
+    }
+
+    public function getSubscribers(string $groupId): array
+    {
+        return $this->httpLayer->get(
+            $this->buildUri($this->endpoint) . "/{$groupId}/subscribers"
+        );
+    }
+
+    public function assignSubscriber(string $groupId, string $subscriberId): array
+    {
+        return $this->httpLayer->post(
+            $this->buildUri('subscribers') . "/{$subscriberId}/groups/{$groupId}/subscribers"
+        );
+    }
+
+    public function unAssignSubscriber(string $groupId, string $subscriberId): array
+    {
+        return $this->httpLayer->delete(
+            $this->buildUri('subscribers') . "/{$subscriberId}/groups/{$groupId}/subscribers"
+        );
+    }
+}
---
+++ src/Endpoints/Segment.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MailerLite\Endpoints;
+
+class Segment extends AbstractEndpoint
+{
+    protected string $endpoint = 'segments';
+
+    public function get(array $params): array
+    {
+        return $this->httpLayer->get(
+            $this->buildUri($this->endpoint, $params)
+        );
+    }
+
+    public function update(string $segmentId, array $params): array
+    {
+        return $this->httpLayer->put(
+            $this->buildUri($this->endpoint) . "/{$segmentId}",
+            $params
+        );
+    }
+
+    public function delete(string $segmentId): array
+    {
+        return $this->httpLayer->delete(
+            $this->buildUri($this->endpoint . "/{$segmentId}")
+        );
+    }
+
+    public function getSubscribers(string $segmentId): array
+    {
+        return $this->httpLayer->get(
+            $this->buildUri($this->endpoint) . "/{$segmentId}/subscribers"
+        );
+    }
+}
